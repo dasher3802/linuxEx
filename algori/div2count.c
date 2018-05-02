13b34c2d57764772801cca28c08bb2e01072c703
dasher3802 <856251@naver.com>
2018-05-02 15:37:28 +0900

    div2count
    update

--- algori/div2count.c
+++ algori/div2count.c
@@ -4,19 +4,23 @@ int function(int num);
 
 int main(void)
 {
-	int num;
+	int num1, num2, sum = 0;
 
-	printf("숫자 입력 : ");
-	scanf("%d", &num);
-	printf("%d\n", function(num));
+	printf("숫자 2번 입력 : ");
+	scanf("%d %d", &num1, &num2);
+	
+	while(num1 <= num2)
+		sum += function(num1++);
+
+	printf("%d\n", sum);
 
 	return 0;
 }
 
 int function(int num)
 {
-	if(num & ~(~0 << 1))
+	if(num & ~(~0 << 1))//비트 오른쪽 한자리만 추출
 		return 1;
 	else
-		return 2 * function(num>>1);
+		return function(num>>1)<<1;//function(num/2)*2
 }
